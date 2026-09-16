3c4733178d6d9c4af3fc405fc858a3f5951db098
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2026-09-16 18:51:58 +0200

    improve: add branch name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/effective-couscous/",
   test: {
     environment: "node",
     include: ["src/**/*.test.ts"],
